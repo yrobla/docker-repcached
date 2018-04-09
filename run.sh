10832f931635da376674a00b22c707e48c9e5706
Matt Morrissette <matt.morrissette@goji.com>
2018-04-09 14:13:36 -0700

    add env variables for config

--- run.sh
+++ run.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
 #create admin account to memcached using SASL
-if [ ! -f /.memcached_admin_created ]; then
+if [ -z "$MEMCACHED_ANON" ]; then
 	/create_memcached_admin_user.sh
+else
+    echo "Memcached running with no auth"
 fi
 
-memcached -u root -vvvv -l 0.0.0.0 -m 64 -P /var/run/memcachedrep.pid -x 127.0.0.1
+memcached -u root \
+    ${MEMCACHED_VERBOSE:--vvvv} \
+    -P ${MEMCACHED_PID_FILE:-/var/run/memcachedrep.pid} \
+    -p ${MEMCACHED_TCP_PORT:-11211} \
+    -U ${MEMCACHED_UDP_PORT:-11211} \
+    -m ${MEMCACHED_MEMORY:-64} \
+    -c ${MEMCACHED_MAX_CONN:-1024} \
+    -t ${MEMCACHED_THREADS:-4} \
+    -x ${MEMCACHED_SLAVE_IP:-127.0.0.1}
